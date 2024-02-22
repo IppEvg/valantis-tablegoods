80e16fb94f0f37ae9217edf23638414b68829b1f
Evgeniy Ippolitov <ippevg@yandex.ru>
2024-02-22 14:34:48 +0300

    Start

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    
+    <v-main>
+  <TableGoods></TableGoods>
+    </v-main>
+    <v-footer fixed  class="footer">
+      <div class="copyright">
+        &copy; 2024 Ippolitov 
+      </div>
+      <div class="links">
+        <p> Phone: 8(903)2452420</p>
+        <div class="messanger">
+        <div><v-btn size="x-small" text href="https://t.me/EvgeniyIppolitov"><img  class="imgMessamger" src="./assets/telegr.svg" alt="Telegram"/> //t.me/Evgeniy Ippolitov</v-btn> </div>
+        </div>
+      </div>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TableGoods from './components/TableGoods.vue';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    TableGoods,
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style lang="scss">
+.footer {
+  background-color: blue;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 2em;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+.messanger {
+  display: flex;
+  max-height: 20px;
+
+  .imgMessamger {
+    height: 20px;
+    min-width: 20px;
+    margin-right: 0.5em;
+  }
+
+}
+
+
+
 </style>
---
+++ src/components/TableGoods.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-container>
+ 
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'TableGoods',
+    data(){
+      return{
+        password:'Valantis',
+      }
+    },
+
+   beforeMount(){
+    const date = new Date().toISOString().slice(0,10).replace(/-/g, '');
+    let params = {
+      method:'POST',
+      headers:{
+        'X-Auth':`md5${this.password}_${date}`
+      },
+      action:'get_fields'
+
+
+    }
+    console.log(params.headers);
+    async function getListGoods(){
+      try {
+        let response = await fetch('http://api.valantis.store:40000/',params)
+    let goods = await response.json();
+    return console.log(goods); 
+      } catch (error) {
+        console.log(error);
+      }
+      
+    }
+    getListGoods()
+    
+     
+
+   }
+  }
+</script>
+
+<style scoped lang="scss">
+</style>
